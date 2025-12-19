246f2af4341f57be091d58ce3c0e9b541dcd4e88
rn7md <mohamed.mos9924@gmail.com>
2025-12-19 20:20:51 +0200

    Delete supbaseClient.js

--- src/supbaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-// You need to replace these with your ACTUAL keys from the Supabase website
-const supabaseUrl = 'https://rpvdchbarzzlcmbressv.supabase.co'
-const supabaseKey = 'sb_publishable_u1JHiphX4ymr0T0xGpszhw_IF9T4URm'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
